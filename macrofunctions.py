29548e6590490efbd715da7d47016922efde9133
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2023-11-19 22:09:02 +0100

    Removed unnecessary code and duplicated dataframes
    - Multiple functions returned the same values, so these have been combined.
    - Reinitialised dataframes so read and write speeds are increased. This results in a considerable decrease in RAM-usage.

--- macrofunctions.py
+++ macrofunctions.py
@@ -8,7 +8,11 @@ import microfunctions as mif
 
 def cacheall(year, sprinttype):
     """
-    TODO: BESCHRIJVING
+    Caches all sessions for offline use for the requested year and sprinttype:
+
+    args:
+    - year (int/str): the year for which the sessions should be loaded
+    - sprinttype (str): the sprinttype for which the sessions should be loaded
     """
     schedule = mif.loadschedule(year)
     for name in schedule.OfficialEventName:
@@ -17,27 +21,32 @@ def cacheall(year, sprinttype):
         print("\x1b[0m")
 
 
-def allcardata(track, year, session, combinedcardata=[], _cardata=None):
+def allcardata(track, year, session, sessiontype, cardata=None):
     """
-    # TODO: Beschrijving
+    # TODO: BESCHRIJVING
     """
     try:
+        combinedcardata = []
+        starttijdcar = perf_counter()
+        print(f'\x1b[32mGetting Car Telemetry Data... {track}')
         for driver in session.drivers:
             lap = 1
             try:
                 while lap <= session.total_laps:
-                    lstdriver, lstyear, lsttrack, lstlap, x = [], [], [], [], 1
+                    lstdriver, lstyear, lsttrack, lstlap, lsttype, x = [], [], [], [], [], 1
                     cardata = session.laps.pick_driver(driver).pick_lap(int(lap)).get_car_data()
                     while x <= cardata.Brake.size:
                         lstdriver.append(driver)
                         lstyear.append(year)
                         lsttrack.append(track)
                         lstlap.append(lap)
+                        lsttype.append(sessiontype)
                         x += 1
                     cardata["driverID"] = lstdriver
                     cardata['year'] = lstyear
                     cardata['gp'] = lsttrack
                     cardata['lap'] = lstlap
+                    cardata['sessiontype'] = lsttype
                     combinedcardata.append(cardata)
                     lap += 1
             except KeyError as ke:
@@ -45,70 +54,43 @@ def allcardata(track, year, session, combinedcardata=[], _cardata=None):
             except ValueError as ve:
                 print(f"\x1b[31m{ve}\x1b[0m")
             except:
-                print('\n\tUNKOWN ERROR!')
-        _cardata = pd.concat(combinedcardata)
-        print(_cardata)
-        return _cardata
+                print('Data not loaded')
+        try:
+            _cardata = pd.concat(combinedcardata)
+            print(f"Tijd: \x1b[31m{(perf_counter() - starttijdcar) * 1000:.0f}ms")
+            return _cardata
+        except UnboundLocalError as ule:
+            print(f"\x1b[31m{ule}\x1b[0m")
+        except AttributeError as ae:
+            print(f"\x1b[31m{ae}\x1b[0m")
+        except TypeError as te:
+            print(f"\x1b[31m{te}\x1b[0m")
     except AttributeError as ae:
         print(f"\x1b[31m{ae}\x1b[0m")
     except ValueError as ve:
         print(f"\x1b[31m{ve}\x1b[0m")
 
 
-def seasoncardata(track, session, year, sessiontype, eventiter=0):
+def alllapdata(name, year, session, sessiontype, lap=1, lapinfo=None):
     """
-    TODO: BESCHRIJVING
-    """
-    try:
-        contentcar = pd.read_csv(f'cardata_{year}.csv')
-        contentcar.isnull().sum()
-        contentcar.shape
-        contentcar.info()
-        print(f'cardata_{year}.csv exists')
-    except FileNotFoundError as fnfe:
-        starttijdcar = perf_counter()
-        print(f'\x1b[32mGetting Car Telemetry Data... {track}')
-        cardata = allcardata(track, year, session)
-        # TODO VRAAG: Data voegt zichzelf toe zonder append?
-        print(f"Tijd: \x1b[31m{(perf_counter() - starttijdcar) * 1000:.0f}ms")
-        eventiter += 1
-        pass
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
-    try:
-        return cardata
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-        return None
-
-
-def writeseasoncardata(track, session, year, sprinttype):
-    """
-    Extension to function above which should be called if cardata has to be written.
+    # TODO: BESCHRIJVING
     """
     try:
-        return seasoncardata(track, session, year, sprinttype)
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
-
-
-def alllapdata(name, year, session, lap=1, combinedlapdata=[], _lapdata=None):
-    """
-    # TODO: Beschrijving
-    """
-    try:
+        combinedlapdata = []
+        starttijdlap = perf_counter()
+        print(f'\x1b[32mGetting Lap Data... {name}')
         while lap <= session.total_laps:
             try:
-                lstyear, lstname, x = [], [], 1
+                lstyear, lstname, lsttype, x = [], [], [], 1
                 lapinfo = mif.loadlap(session, lap)
                 while x <= lapinfo.DriverNumber.size:
                     lstyear.append(year)
                     lstname.append(name)
+                    lsttype.append(sessiontype)
                     x += 1
                 lapinfo['year'] = lstyear
                 lapinfo['gp'] = lstname
+                lapinfo['sessiontype'] = lsttype
             except KeyError as ke:
                 print(f"\x1b[31m{ke}\x1b[0m")
             except ValueError as ve:
@@ -117,121 +99,50 @@ def alllapdata(name, year, session, lap=1, combinedlapdata=[], _lapdata=None):
             # print(lapinfo)
             lap += 1
         _lapdata = pd.concat(combinedlapdata)
-        print(_lapdata)
+        print(f"Tijd: \x1b[31m{(perf_counter() - starttijdlap) * 1000:.0f}ms")
         return _lapdata
     except AttributeError as ae:
         print(f"\x1b[31m{ae}\x1b[0m")
+    except TypeError as te:
+            print(f"\x1b[31m{te}\x1b[0m")
     except:
-        print('\t\tSOMETHING WENT WRONG')
+        print('\t\tData not loaded')
 
 
-def seasonlapdata(track, session, year, sessiontype, eventiter=0, combinedlap=[]):
-    try:
-        contentlap = pd.read_csv(f'lapdata_{year}.csv')
-        contentlap.isnull().sum()
-        contentlap.shape
-        contentlap.info()
-        print(f'lapdata_{year}.csv exists')
-    except FileNotFoundError as fnfe:
-        starttijdlap = perf_counter()
-        print(f'\x1b[32mGetting Lap Data... {track}')
-        lapdata = alllapdata(track, year, session)
-        print(f"Tijd: \x1b[31m{(perf_counter() - starttijdlap) * 1000:.0f}ms")
-        if lapdata.empty:
-            pass
-        else:
-            combinedlap.append(lapdata)
-        eventiter += 1
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
+def allweatherdata(name, year, session, sessiontype, lap=1, weerdata=None):
     """
-    TODO: Ik krijg hier dubbele data terug doordat ik 'lapdata' toevoeg aan een lijst (2023).
-    Hoe zorg ik ervoor dat ik dit niet krijg zonder dat lapdata 'None' wordt door
-    de laatste races?
+    TODO: Verander dit naar weerdata van de sessie zelf {name, session, year} extra values {lap}
     """
     try:
-        if combinedlap != []:
-            return pd.concat(combinedlap)
-        elif lapdata.empty:
-            return None
-        else:
-            return lapdata
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-        return None
-
-
-def writeseasonlapdata(track, session, year, sprinttype):
-    """
-    Extension to function above which should be called if lapdata has to be written.
-    """
-    try:
-        return seasonlapdata(track, session, year, sprinttype)
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
-
-
-
-def allweatherdata(name, year, session, combinedweatherdata=[], lap=1, _weatherdata=None):
-    # TODO: Verander dit naar weerdata van de sessie zelf {name, session, year} extra values {lap}
-    # DRIVER IS HIER NIET NODIG AANGEZIEN DE DATA HIER HETZELFDE IS.
-    try:
+        combinedweatherdata = []
+        starttijdweather = perf_counter()
+        print(f'\x1b[32mGetting Weather Data... {name}')
         try:
-            lstyear, lstlap, lstname, x = [], [], [], 1
+            lstyear, lstlap, lstname, lsttype, x = [], [], [], [], 1
             weerdata = session.weather_data
             # TODO: Krijg af en toe lege dataframes terug?
             while x <= weerdata.AirTemp.size:
                 lstyear.append(year)
                 lstlap.append(lap)
                 lstname.append(name)
+                lsttype.append(sessiontype)
                 x += 1
             weerdata['year'] = lstyear
             weerdata['lap'] = lstlap
             weerdata['gp'] = lstname
+            weerdata['sessiontype'] = lsttype
             combinedweatherdata.append(weerdata)
         except KeyError as ke:
             print(f"\x1b[31m{ke}\x1b[0m")
         except ValueError as ve:
             print(f"\x1b[31m{ve}\x1b[0m")
+        except:
+            print("Data Not Loaded")
         lap += 1
         _weatherdata = pd.concat(combinedweatherdata)
-        print(_weatherdata)
+        print(f"Tijd: \x1b[31m{(perf_counter() - starttijdweather) * 1000:.0f}ms")
         return _weatherdata
     except AttributeError as ae:
         print(f"\x1b[31m{ae}\x1b[0m")
-
-
-def seasonweatherdata(track, session, year, sessiontype):
-    try:
-        contentweather = pd.read_csv(f'weatherdata_{year}.csv')
-        contentweather.isnull().sum()
-        contentweather.shape
-        contentweather.info()
-        print(f'weatherdata_{year}.csv exists')
-    except FileNotFoundError as fnfe:
-        starttijdweather = perf_counter()
-        print(f'\x1b[32mGetting Weather Data... {track}')
-        weatherdata = allweatherdata(track, year, session)
-        # TODO VRAAG: Data voegt zichzelf toe zonder append?
-        print(f"Tijd: \x1b[31m{(perf_counter() - starttijdweather) * 1000:.0f}ms")
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
-    try:
-        return weatherdata
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-        return None
-
-
-def writeseasonweatherdata(track, session, year, sprinttype):
-    """
-    Extension to function above which should be called if lapdata has to be written.
-    """
-    try:
-        return seasonweatherdata(track, session, year, sprinttype)
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-    except AttributeError as ae:
-        print(f"\x1b[31m{ae}\x1b[0m")
+    except TypeError as te:
+        print(f"\x1b[31m{te}\x1b[0m")
