f946667492fd3945bc03fb5489bfd230abf33256
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2023-12-01 06:30:23 +0100

    Added datadirectories

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
 class Settings:
     clear_cache: bool = False
+    clear_deep: bool = False
+    micro_data_dir: Path = Path(__file__).parent / 'Data' / 'Microdata'
+    macro_data_dir: Path = Path(__file__).parent / 'Data' / 'Macrodata'
+    master_data_dir: Path = Path(__file__).parent / 'Data' / 'Masterdata'
+    root_dir: Path = Path(__file__).parent
+
+settings = Settings()
