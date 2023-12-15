6a6266a4a506946cabab2be163eddb5572b27c28
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2023-12-15 21:27:25 +0100

    Progress

--- predictions_r/exampleGermanCredit.r
+++ predictions_r/exampleGermanCredit.r
@@ -2,16 +2,20 @@ if(!require('caret')) {
   install.packages('caret')
   library('caret')
 }
+if(!require('tidyverse')) {
+  install.packages('tidyverse')
+  library('tidyverse')
+}
 
 data(GermanCredit)
 
 set.seed(200)
 trainIndex <- createDataPartition(GermanCredit$Amount, p = 0.85, list=FALSE)
+# Used in Confusion Matrices
 train_data <- GermanCredit[trainIndex,]
 test_data <- GermanCredit[-trainIndex,]
 
 summary(model <- train(Amount ~ ., data=train_data, method="lm"))
-
 
 predicted_creditscore <- data.frame(GermanCredit$Amount)
 names(predicted_creditscore)[names(predicted_creditscore)=="GermanCredit.Amount"] <- "CS_amount"
@@ -19,4 +23,5 @@ predicted_creditscore['CS_predicted'] <- predict(model, newdata=GermanCredit)
 
 plot(predicted_creditscore)
 
-summary(lm(CS_predicted ~ ., data=predicted_creditscore))
+summary(linmod <- lm(CS_predicted ~ ., data=predicted_creditscore))
+plot(linmod)
--- predictions_r/results.r
+++ predictions_r/results.r
@@ -2,40 +2,63 @@ if(!require('caret')) {
   install.packages('caret')
   library('caret')
 }
-
 if(!require('nnet')) {
   install.packages('nnet')
   library('nnet')
 }
-
-if(!require('neuralnet')) {
-  install.packages('neuralnet')
-  library('neuralnet')
+if(!require('tidyverse')) {
+  install.packages('tidyverse')
+  library('tidyverse')
 }
 
-if(!require('randomGLM')) {
-  install.packages('randomGLM')
-  library('randomGLM')
-}
-
-dataset <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/clean_results.csv', 
+dataset <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/data/clean_results.csv', 
                                    sep = ';')))
-driver_standings <- try(data.frame(read.csv('/Users/delano/Library/CloudStorage/OneDrive-StichtingHogeschoolUtrecht/Jaar 2/Semester 3/Speedway Dynamics/Speedway Dynamics - Datasemester/DATA/3_Cleaned/clean_driver_standings.csv', 
+driver_standings <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/data/clean_driver_standings.csv', 
                                             sep = ';')))
 
-set.seed(10)
+set.seed(40)
 train_index <-  createDataPartition(dataset$resultId, p = 0.7, list = FALSE)
 train_data <- dataset[train_index,]
 test_data <- dataset[-train_index,]
 
-data_af_na <- na.omit(dataset)
-model_results <- train(positionOrder ~ statusId + points + grid, data = data_af_na, method = 'lm')
-summary(model_results)
+# train_data_af_na <- train_data
+train_data_af_na <- na.omit(train_data)
+test_data_af_na <- na.omit(test_data)
 
-results <- data.frame(data_af_na$positionOrder)
-names(results)[names(results)=='data_af_na.positionOrder'] <- 'pos_real'
-results['pos_pred'] <- predict(model_results, newdata = data_af_na)
+################### Random Forest (rf) ###################
+# summary(model_rf <- train(positionOrder ~ points + grid, data = train_data_af_na, method = 'rf'))
+# saveRDS(model_rf, file = '/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_rf.rda')
+model_rf <- readRDS('/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_rf.rda')
 
-plot(results)
+################### Generalised Linear Model (glm) ###################
+summary(model_glm <- train(positionOrder ~ points + grid, data = train_data_af_na, method = 'glm'))
+saveRDS(model_glm, file = '/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_glm.rda')
+model_glm <- readRDS('/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_glm.rda')
 
-confusionMatrix(predict(model_results, newdata = test_data), test_data$positionOrder)
+################### Negative Binomial Generalized Linear Model (glm.nb) ###################
+summary(model_glmnb <- train(positionOrder ~ points + grid, data = train_data_af_na, method = 'glm.nb'))
+saveRDS(model_glmnb, file = '/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_glmnb.rda')
+model_glmnb <- readRDS('/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_glmnb.rda')
+
+################### Parallel Random Forest (parRF) ###################
+# summary(model_parRF <- train(positionOrder ~ points + grid, data = train_data_af_na, method = 'parRF'))
+# saveRDS(model_parRF, file = '/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_parRF.rda')
+model_parRF <- readRDS('/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_parRF.rda')
+
+################### Quantile Random Forest (qrf) ###################
+# summary(model_qrf <- train(positionOrder ~ points + grid, data = train_data_af_na, method = 'qrf'))
+# saveRDS(model_qrf, file = '/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_qrf.rda')
+model_qrf <- readRDS('/Users/delano/Documents/GitHub/Data_Drivers/predictions_r/model_qrf.rda')
+
+################### Predictions ###################
+prediction_results <- data.frame(test_data_af_na$positionOrder)
+names(prediction_results)[names(prediction_results)=='test_data_af_na.positionOrder'] <- 'pos_real'
+prediction_results['pos_pred_rf'] <- predict(model_rf, newdata = test_data_af_na)
+prediction_results['pos_pred_glm'] <- predict(model_glm, newdata = test_data_af_na)
+prediction_results['pos_pred_glmnb'] <- predict(model_glmnb, newdata = test_data_af_na)
+prediction_results['pos_pred_parRF'] <- predict(model_parRF, newdata = test_data_af_na)
+prediction_results['pos_pred_qrf'] <- predict(model_qrf, newdata = test_data_af_na)
+
+plot(prediction_results)
+
+# confusionMatrix(predict(model_results, newdata = test_data), test_data$positionOrder)
