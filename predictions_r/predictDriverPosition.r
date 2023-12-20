82df6799aa61f3cffa06549b515e02b744b4ffeb
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2023-12-20 21:18:27 +0100

    Progress

--- predictions_r/predictDriverPosition.r
+++ predictions_r/predictDriverPosition.r
@@ -1,8 +1,3 @@
-################### DEFINE DATASETS ###################
-dataset <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/data/clean_results.csv', 
-                                   sep = ';')))
-set <- na.omit(dataset)
-
 ################### IMPORT LIBRARIES ###################
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 library(quantregForest)
@@ -10,22 +5,30 @@ library(caret)
 library(cvms)
 library(tibble)
 
+################### DEFINE DATASETS ###################
+dataset <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/data/clean_results.csv', 
+                                   sep = ';')))
+set <- na.omit(dataset)
+trainIndex <- createDataPartition(set$resultsId, p=0.7, list=FALSE)
+trainData <- set[trainIndex,]
+testData <- set[-trainIndex,]
+
 ################### Train Models ###################
-model_qrf <- quantregForest(x = set, y = set$positionOrder, nthreads = 2)
-model_lm <- lm(positionOrder ~ points + grid + fastestLapSpeed + statusId, data=set)
-model_rf <- randomForest(positionOrder ~ points + grid + fastestLapSpeed + statusId, data=set, proximity=TRUE)
+summary(model_qrf <- quantregForest(x = set, y = set$positionOrder, nthreads = 4))
+summary(model_lm <- lm(positionOrder ~ points + grid + fastestLapSpeed + statusId, data=set))
+summary(model_rf <- randomForest(positionOrder ~ points + grid + fastestLapSpeed + statusId, data=set, proximity=TRUE))
 
 ################### Make Predictions ###################
 prediction_results <- data.frame(set$positionOrder)
 names(prediction_results)[names(prediction_results)=='set.positionOrder'] <- 'pos_real'
-prediction_results['pos_qrf'] <- data.frame(abs(round(predict(model_qrf, newdata = set), digits = 0)))
-prediction_results['pos_lm'] <- data.frame(abs(round(predict(model_lm, newdata = set), digits = 0)))
-prediction_results['pos_rf'] <- data.frame(abs(round(predict(model_rf, newdata = set), digits = 0)))
+prediction_results['pos_qrf'] <- data.frame(abs(round(predict(model_qrf, newdata=set), digits=0)))
+prediction_results['pos_lm'] <- data.frame(abs(round(predict(model_lm, newdata=set), digits=0)))
+prediction_results['pos_rf'] <- data.frame(abs(round(predict(model_rf, newdata=set), digits=0)))
 # plot(prediction_results)
 
 ################### "Gecombineerd Gemiddelde" ###################
 mean_results <- data.frame(set$positionOrder)
-mean_results['pos_predicted'] <- abs(round(rowMeans(prediction_results, na.rm=TRUE), digits = 0))
+mean_results['pos_predicted'] <- abs(round(rowMeans(prediction_results, na.rm=TRUE), digits=0))
 names(mean_results)[names(mean_results)=='set.positionOrder'] <- 'pos_real'
 # plot(mean_results)
 
--- predictions_r/predictLaptime.R
+++ predictions_r/predictLaptime.R
@@ -1,30 +1,31 @@
+################### IMPORT LIBRARIES ###################
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+library(caret)
+library(cvms)
+library(tibble)
+library(frbs)
+
 ################### DEFINE DATASETS ###################
 dataset <- try(data.frame(read.csv('/Users/delano/Documents/GitHub/Data_Drivers/data/clean_lap_times.csv', 
                                    sep = ';')))
 set <- na.omit(dataset)
-
-################### IMPORT LIBRARIES ###################
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-library(quantregForest)
-library(caret)
-library(cvms)
-library(tibble)
+trainIndex <- createDataPartition(set$raceId, p=0.69, list=FALSE)
+trainData <- set[trainIndex,]
+testData <- set[-trainIndex,]
 
 ################### Train Models ###################
-model_qrf <- quantregForest(x = set, y = set$milliseconds, nthreads = 2)
-model_lm <- lm(milliseconds ~ driverId + raceId + position, data=set)
-model_rf <- randomForest(milliseconds ~ driverId + raceId + position, data=set, proximity=TRUE)
+summary(model_glm <- try(train(milliseconds ~ driverId + raceId + position, data=trainData, method='glm')))
+summary(model_DENFIS <- try(train(milliseconds ~ driverId + raceId + position, data=trainData, method='DENFIS')))
 
 ################### Make Predictions ###################
-prediction_results <- data.frame(set$milliseconds)
-names(prediction_results)[names(prediction_results)=='set.milliseconds'] <- 'mil_real'
-prediction_results['mil_qrf'] <- data.frame(abs(round(predict(model_qrf, newdata = set), digits = 0)))
-prediction_results['mil_lm'] <- data.frame(abs(round(predict(model_lm, newdata = set), digits = 0)))
-prediction_results['mil_rf'] <- data.frame(abs(round(predict(model_rf, newdata = set), digits = 0)))
+prediction_results <- data.frame(testData$milliseconds)
+names(prediction_results)[names(prediction_results)=='testData.milliseconds'] <- 'mil_real'
+prediction_results['mil_glm'] <- data.frame(abs(round(predict(model_glm, newdata=testData), digits=0)))
+prediction_results['mil_DENFIS'] <- data.frame(abs(round(predict(model_DENFIS, newdata=testData), digits=0)))
 plot(prediction_results)
 
 ################### "Gecombineerd Gemiddelde" ###################
-mean_results <- data.frame(set$positionOrder)
-mean_results['mil_predicted'] <- abs(round(rowMeans(prediction_results, na.rm=TRUE), digits = 0))
-names(mean_results)[names(mean_results)=='set.milliseconds'] <- 'mil_real'
+mean_results <- data.frame(testData$milliseconds)
+mean_results['mil_predicted'] <- abs(round(rowMeans(prediction_results, na.rm=TRUE), digits=0))
+names(mean_results)[names(mean_results)=='testData.milliseconds'] <- 'mil_real'
 plot(mean_results)
