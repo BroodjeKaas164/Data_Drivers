5d26107824239caa06f76dec82fbb9bf32e531cd
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2023-12-31 17:14:27 +0100

    Update settings.py

--- getdata_python/settings.py
+++ getdata_python/settings.py
@@ -6,9 +6,9 @@ from pathlib import Path
 class Settings:
     clear_cache: bool = False
     clear_deep: bool = False
-    micro_data_dir: Path = Path(__file__).parent / 'Data' / 'Microdata'
-    macro_data_dir: Path = Path(__file__).parent / 'Data' / 'Macrodata'
-    master_data_dir: Path = Path(__file__).parent / 'Data' / 'Masterdata'
+    micro_data_dir: Path = Path(__file__).parent.parent / 'Data' / 'Microdata'
+    macro_data_dir: Path = Path(__file__).parent.parent / 'Data' / 'Macrodata'
+    master_data_dir: Path = Path(__file__).parent.parent / 'Data' / 'Masterdata'
     root_dir: Path = Path(__file__).parent.parent
 
 settings = Settings()
