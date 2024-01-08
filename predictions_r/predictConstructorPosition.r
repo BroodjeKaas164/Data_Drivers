fa63bb38704eeeba01206ab602c62e3d454b03dc
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2024-01-08 08:49:32 +0100

    Adjusted Predicted Factor

--- predictions_r/predictConstructorPosition.r
+++ predictions_r/predictConstructorPosition.r
@@ -6,15 +6,14 @@ source('predictions_r/sourceModelFunctions.r', chdir=TRUE)
 ################### DEFINE PARAMETERS ###################
 # SETTINGS
 set.seed(69)
-use_models <- c('glm', 'lm', 'glm.nb')
+use_models <- c('glm', 'lm', 'BstLm')
 optimise_model <- 'glm'
 decimals <- 0
-p_factor <- 'points'
+p_factor <- 'position'
 
 # Import datasets
-dataset <- try(data.frame(read.csv('data/clean_constructor_standings.csv', 
-                                   sep=';')))
-alldata <- data_splitter(dataset, 0.8)
+dataset <- try(data.frame(read.csv('data/clean_constructor_standings.csv', sep=';')))
+alldata <- data_splitter(dataset, 0.6)
 trainers <- alldata[['trainers']]
 testers <- alldata[['testers']]
 
@@ -23,7 +22,7 @@ train_models <- function(models, trainData, model_dict=list()) {
   for (model in models) {
     model_name <- paste0('model_', model)
     print(model_name)
-    model <- try(train(points ~ raceId + constructorId + position + wins, data=trainData, method=model))
+    model <- try(train(position ~ raceId + constructorId + points + wins, data=trainData, method=model))
     try(model_dict[[model_name]] <- model)
     print(summary(model))
   }
@@ -32,10 +31,10 @@ train_models <- function(models, trainData, model_dict=list()) {
 trained_models <- train_models(use_models, trainers)
 
 ################### PREDICTION RESULTS ###################
-plot(trainResultsAssigned <- assign_results(trainers, use_models, trained_models))
+# plot(trainResultsAssigned <- assign_results(trainers, use_models, trained_models))
 
 ################### COMBINED MEANDIAN ###################
-plot(trainResultsMeandian <- combined_meandian(trainers, trainResultsAssigned))
+# plot(trainResultsMeandian <- combined_meandian(trainers, trainResultsAssigned))
 
 ################### MODEL REWORK ###################
 plot(final <- reworked_results(testers))
