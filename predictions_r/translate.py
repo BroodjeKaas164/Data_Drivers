04eb02d820ffa41c58871ae262e2b0752a244931
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2024-01-08 08:50:24 +0100

    Delete translate.py

--- predictions_r/translate.py
+++
@@ -1,8 +0,0 @@
-def train_models():
-    models = ['glm', 'parRF', 'rf']
-    listModels = []
-    for model in models:
-        listModels.append(f'model_{model}')
-    print(listModels)
-
-train_models()
