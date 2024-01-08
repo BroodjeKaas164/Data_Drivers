db78323c23ceeb0d5048bf23a9bb584ed1af484a
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2024-01-08 08:50:11 +0100

    Adjusted Train Methods

--- predictions_r/predictLaptime.R
+++ predictions_r/predictLaptime.R
@@ -6,14 +6,13 @@ source('predictions_r/sourceModelFunctions.r', chdir=TRUE)
 ################### DEFINE PARAMETERS ###################
 # SETTINGS
 set.seed(69)
-use_models <- c('glm', 'glm.nb')
+use_models <- c('glm', 'glm.nb', 'Bstlm')
 optimise_model <- 'glm'
 decimals <- 0
 p_factor <- 'milliseconds'
 
 # Import datasets
-dataset <- try(data.frame(read.csv('data/clean_lap_times.csv', 
-                                   sep = ';')))
+dataset <- try(data.frame(read.csv('data/clean_lap_times.csv', sep = ';')))
 alldata <- data_splitter(dataset, 0.8)
 trainers <- alldata[['trainers']]
 testers <- alldata[['testers']]
@@ -32,10 +31,10 @@ train_models <- function(models, trainData, model_dict=list()) {
 trained_models <- train_models(use_models, trainers)
 
 ################### PREDICTION RESULTS ###################
-plot(trainResultsAssigned <- assign_results(trainers, use_models, trained_models))
+# plot(trainResultsAssigned <- assign_results(trainers, use_models, trained_models))
 
 ################### COMBINED MEANDIAN ###################
-plot(trainResultsMeandian <- combined_meandian(trainers, trainResultsAssigned))
+# plot(trainResultsMeandian <- combined_meandian(trainers, trainResultsAssigned))
 
 ################### MODEL REWORK ###################
 plot(final <- reworked_results(testers))
