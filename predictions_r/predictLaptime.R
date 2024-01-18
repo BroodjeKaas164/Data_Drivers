888fc898d131716e55ad0d2c4af4a40f14696808
Delano Tjon-A-Njoek <94830088+BroodjeKaas164@users.noreply.github.com>
2024-01-18 17:41:04 +0100

    Added fill value on empty

--- predictions_r/predictLaptime.R
+++ predictions_r/predictLaptime.R
@@ -6,16 +6,16 @@ source('predictions_r/sourceModelFunctions.r', chdir=TRUE)
 ################### DEFINE PARAMETERS ###################
 # SETTINGS
 set.seed(69)
-use_models <- c('glm', 'glm.nb')
+use_models <- c('glm', 'glm.nb', 'lm')
 optimise_model <- 'glm'
 decimals <- 0
 p_factor <- 'milliseconds'
 
 # Import datasets
 dataset <- try(data.frame(read.csv('data/clean_lap_times.csv', sep = ';')))
-alldata <- data_splitter(dataset, 0.6)
-trainers <- alldata[['trainers']]
-testers <- alldata[['testers']]
+# alldata <- data_splitter(dataset, 0.5)
+trainers <- data_splitter(dataset, 0.2)[['trainers']]
+testers <- data_splitter(dataset, 0.85)[['testers']]
 
 ################### TRAIN MODELS ###################
 train_models <- function(models, trainData, model_dict=list()) {
@@ -31,10 +31,10 @@ train_models <- function(models, trainData, model_dict=list()) {
 trained_models <- train_models(use_models, trainers)
 
 ################### PREDICTION RESULTS ###################
-# plot(trainResultsAssigned <- assign_results(trainers, use_models, trained_models))
+# plot(trainResultsAssigned <- assign_results(testers, use_models, trained_models))
 
 ################### COMBINED MEANDIAN ###################
-# plot(trainResultsMeandian <- combined_meandian(trainers, trainResultsAssigned))
+# plot(trainResultsMeandian <- combined_meandian(testers, trainResultsAssigned))
 
 ################### MODEL REWORK ###################
 # plot(final <- reworked_results(testers))
@@ -42,4 +42,4 @@ trained_models <- train_models(use_models, trainers)
 datanew <- data.frame(dataset)
 final <- reworked_results(dataset)
 datanew[["pred_laptime"]] <- final$p_optimised
-write.csv(datanew, "predictions_r/pred_laptimes.csv", row.names=FALSE)
+write.csv(datanew, "predictions_r/pred_laptimes.csv", row.names=FALSE, na="NA")
